2a9dc0747d9fe60bc1420e7ee5ac63a275f6ada0
VanikSarajya <vaniksarajyan@gmail.com>
2018-08-17 15:56:26 +0400

    student migration

--- migrations/20180807112749-create-students.js
+++ migrations/20180807112749-create-students.js
@@ -20,14 +20,10 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       createdAt: {
-        allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       },
       updatedAt: {
-        allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
     });
   },
